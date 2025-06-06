020c66017832897e836a5e55e74d72ebf2592fac
Will Hao <williamhao99@gmail.com>
2025-06-05 23:10:10 -0500

    Changed widget path directories for organization.

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 // components/Hero.js
-import SpotifyWidget from './SpotifyWidget';
-import ChessWidget from './ChessWidget';
-import ClashWidget from './ClashWidget';
+import SpotifyWidget from './widgets/SpotifyWidget';
+import ChessWidget from './widgets/ChessWidget';
+import ClashWidget from './widgets/ClashWidget';
 
 export default function Hero({
   heroText = "I'm Will — welcome to my website!",
